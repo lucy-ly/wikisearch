18c31e4dea9d5cb8dee60c4287bf3b28c042e092
Lucy Ly <lucyly.student@careerdevs.com>
2019-04-25 17:09:24 +0000

    add style.css, script.js, index.html wikisearch API

---
+++ script.js
@@ -0,0 +1,42 @@
+/* global fetch */
+
+const response = ["apple", "banana", "orange"];
+
+
+// Click handler
+function handleClick() {
+    console.log("Button is clicked");
+
+    makeAPIcall();
+
+}
+
+function makeAPIcall() {
+    const input = document.getElementById("input").value
+    console.log(input);
+    const url = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=${input}&limit=10&namespace=0&format=json`;
+
+
+    fetch(url)
+        .then(function(response) {
+            return response.json()
+        }).then(function(response) {
+            console.log(response);
+            displayResults(response[1]);
+
+        });
+
+}
+
+// This function displays results(a array) on the page as a list
+function displayResults(response) {
+
+    const newResponse = response.map(function(item) {
+        console.log(item);
+        return "<li>" + "<a href=#>" + item + "</a>" + "</li>"
+    });
+    const joinedResponse = newResponse.join("\n");
+    console.log(joinedResponse);
+
+    document.getElementById("results").innerHTML = joinedResponse;
+}
